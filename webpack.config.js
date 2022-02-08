4c5b244ae4fded04446d781bb9ff6a794f55845c
Max-Dov <maksimdovydenko@protonmail.com>
2022-02-08 03:16:22 +0300

    speedrun project setup (58 min)

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    target: 'node',
+    entry: './src/bot.ts',
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                exclude: [
+                    /node_modules/,
+                ],
+                include: [
+                    path.resolve(__dirname, 'src'),
+                ],
+                test: /\.ts$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env'],
+                            ['@babel/preset-typescript'],
+                        ],
+                        plugins: ["@babel/plugin-proposal-class-properties"],
+                    }
+                }
+            }
+        ]
+    },
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js'
+    },
+    externals: [
+        {
+            'utf-8-validate': 'commonjs utf-8-validate',
+            bufferutil: 'commonjs bufferutil',
+        },
+    ],
+}
